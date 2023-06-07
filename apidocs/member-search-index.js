132de3d706ca12be400e8e08744729158590b729
CI Server <ci@example.com>
2023-06-07 20:56:45 +0000

    Site checkin for project OpenWMS.org COMMON: Base Services

---
+++ apidocs/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"org.openwms.common","c":"CommonStarter","l":"CommonStarter()","u":"%3Cinit%3E()"},{"p":"org.openwms.common","c":"CommonStarter","l":"main(String[])","u":"main(java.lang.String[])"}];updateSearchResults();
---
+++ apidocs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"org.openwms.common","l":"CommonStarter"}];updateSearchResults();
